bdc06632276bdf2e6a51c44ca92f0c229fd86dcd
kumarlalait79 <luckyarya0101@gmail.com>
2026-03-03 14:43:31 +0530

    todo store

--- src/app/features/todos/store/todo.store.ts
+++ src/app/features/todos/store/todo.store.ts
@@ -13,14 +13,38 @@ type TodoStore = {
   setFilter: (filter: FilterType) => void;
 };
 
-export const useTodoStore = create<TodoStore>(() => ({
+export const useTodoStore = create<TodoStore>((set, get) => ({
   // intial state
   todos: [],
   filter: "all",
 
   // actions
-  addTodo: () => {},
-  toggleTodo: () => {},
-  deleteTodo: () => {},
-  setFilter: () => {},
+  addTodo: (title) => {
+    set((state) => ({
+      todos: [
+        {
+          id: crypto.randomUUID(),
+          title,
+          completed: false,
+          createdAt: Date.now(),
+        },
+        ...state.todos,
+      ],
+    }));
+  },
+  toggleTodo: (id) => {
+    set((state) => ({
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    }));
+  },
+  deleteTodo: (id) => {
+    set((state) => ({
+        todos: state.todos.filter((todo) => todo.id !== id)
+    }))
+  },
+  setFilter: (filter) => {
+    set({filter})
+  },
 }));
